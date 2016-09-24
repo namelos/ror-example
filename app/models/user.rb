e48342629b4d26a93a352ea0799ba9ff36a42714
Yifan Wang <namelos@outlook.com>
2016-09-24 23:32:23 +0800

    add token methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save { email.downcase! }
 
   validates :name,
@@ -22,5 +24,18 @@ class User < ApplicationRecord
         BCrypt::Engine.cost
 
     BCrypt::Password.create(string, cost: cost)
+  end
+
+  def User.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  def authenticated?(remember_token)
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
   end
 end

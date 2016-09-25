6c0ca5a5a99c8ce44df4d31d198c8d60c4b57c96
Yifan Wang <namelos@outlook.com>
2016-09-25 16:35:09 +0800

    avoid user logout through different browsers

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -13,5 +13,12 @@ class ActiveSupport::TestCase
     !session[:user_id].nil?
   end
 
+  def log_in_as(user)
+    session[:user_id] = user.id
+  end
   # Add more helper methods to be used by all tests here...
 end
+
+# class ActionDispatch::IntegrationTest
+#   def log_in_as
+# end

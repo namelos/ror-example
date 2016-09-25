aaf69758d33b7debfcd622955763f0b90a91f925
Yifan Wang <namelos@outlook.com>
2016-09-25 18:38:44 +0800

    add avatar back

--- test/integration/users_edit_test.rb
+++ test/integration/users_edit_test.rb
@@ -9,9 +9,9 @@ class UsersEditTest < ActionDispatch::IntegrationTest
     get edit_user_path(@user)
     assert_template 'users/edit'
     patch user_path(@user), params: { user: { name: '',
-                                               email: 'foo@invalid',
-                                               password: 'foo',
-                                               password_confirmation: 'bar' }}
+                                              email: 'foo@invalid',
+                                              password: 'foo',
+                                              password_confirmation: 'bar' }}
     assert_template 'users/edit'
   end
 end

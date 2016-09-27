eec87189a53a6de0ac218acd688a0d6625a2c894
Yifan Wang <namelos@outlook.com>
2016-09-27 23:48:12 +0800

    add account activate mailer

--- config/routes.rb
+++ config/routes.rb
@@ -13,11 +13,13 @@ Rails.application.routes.draw do
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
 
+  resources :users
+  resources :account_activations, only: [:edit]
+
   # get 'static_pages/home'
   # get 'static_pages/help'
   # get 'static_pages/about'
   # get 'static_pages/contact'
 
-  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

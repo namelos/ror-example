da24fc55eab6ef2dbe7c42ae78adc1f06f63e50a
Yifan Wang <namelos@outlook.com>
2016-10-01 13:13:21 +0800

    finish account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,6 +45,15 @@ class User < ApplicationRecord
     BCrypt::Password.new(digest).is_password?(token)
   end
 
+  def activate
+    update_attribute(:activated, true)
+    update_attribute(:activated_at, Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
   def forget
     update_attribute(:remember_digest, nil)
   end
